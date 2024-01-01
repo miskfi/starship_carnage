fb0edbfccf3a10c2aca3872022a064f39cf6dde7
Filip Miskarik <miskfilip@gmail.com>
2024-01-01 18:49:26 +0100

    Sound refactor

--- src/components/sound.ts
+++ src/components/sound.ts
@@ -7,46 +7,56 @@ export class SoundSystem extends Colfio.Component
     onInit()
     {
         this.subscribe(
-            Messages.PROJECTILE_SHOT,
-            Messages.MAIN_MENU,
+            Messages.BUTTON_CHANGE,
+            Messages.ENEMY_DESTROYED,
             Messages.GAME_OVER,
             Messages.LEVEL_FINISHED,
             Messages.LEVEL_START,
-            Messages.BUTTON_CHANGE,
-            Messages.ENEMY_DESTROYED,
-            Messages.PLAYER_HIT
+            Messages.MAIN_MENU,
+            Messages.PLAYER_HIT,
+            Messages.PROJECTILE_SHOT
         );
     }
 
     onMessage(msg: Colfio.Message): any
     {
-        if (msg.action === Messages.PROJECTILE_SHOT)
-            sound.play(GameAssets.SOUND_SHOT, {volume: 0.4});
-        else if (msg.action === Messages.MAIN_MENU)
+        switch (msg.action)
         {
-            sound.stop(GameAssets.SOUND_MUSIC_GAME);
-            sound.play(GameAssets.SOUND_MUSIC_MENU, {loop: true, volume: 0.5});
+            case Messages.BUTTON_CHANGE:
+                sound.play(GameAssets.SOUND_BUTTON_CHANGE, {volume: 1});
+                break;
+
+            case Messages.ENEMY_DESTROYED:
+                sound.play(GameAssets.SOUND_ENEMY_DESTROYED, {volume: 0.3});
+                break;
+
+            case Messages.GAME_OVER:
+                sound.stop(GameAssets.SOUND_MUSIC_GAME);
+                sound.play(GameAssets.SOUND_GAME_OVER, {volume: 0.5});
+                break;
+
+            case Messages.LEVEL_FINISHED:
+                sound.stop(GameAssets.SOUND_MUSIC_GAME);
+                sound.play(GameAssets.SOUND_LEVEL_FINISHED, {volume: 0.7});
+                break;
+
+            case Messages.LEVEL_START:
+                sound.stop(GameAssets.SOUND_MUSIC_MENU);
+                sound.play(GameAssets.SOUND_MUSIC_GAME, {loop: true, volume: 0.3});
+                break;
+
+            case Messages.MAIN_MENU:
+                sound.stop(GameAssets.SOUND_MUSIC_GAME);
+                sound.play(GameAssets.SOUND_MUSIC_MENU, {loop: true, volume: 0.5});
+                break;
+
+            case Messages.PLAYER_HIT:
+                sound.play(GameAssets.SOUND_PLAYER_HIT, {volume: 0.5});
+                break;
+
+            case Messages.PROJECTILE_SHOT:
+                sound.play(GameAssets.SOUND_SHOT, {volume: 0.4});
+                break;
         }
-        else if (msg.action === Messages.LEVEL_START)
-        {
-            sound.stop(GameAssets.SOUND_MUSIC_MENU);
-            sound.play(GameAssets.SOUND_MUSIC_GAME, {loop: true, volume: 0.3});
-        }
-        else if (msg.action === Messages.GAME_OVER)
-        {
-            sound.stop(GameAssets.SOUND_MUSIC_GAME);
-            sound.play(GameAssets.SOUND_GAME_OVER, {volume: 0.5});
-        }
-        else if (msg.action === Messages.LEVEL_FINISHED)
-        {
-            sound.stop(GameAssets.SOUND_MUSIC_GAME);
-            sound.play(GameAssets.SOUND_LEVEL_FINISHED, {volume: 0.7});
-        }
-        else if (msg.action === Messages.BUTTON_CHANGE)
-            sound.play(GameAssets.SOUND_BUTTON_CHANGE, {volume: 1})
-        else if (msg.action === Messages.ENEMY_DESTROYED)
-            sound.play(GameAssets.SOUND_ENEMY_DESTROYED, {volume: 0.3})
-        else if (msg.action === Messages.PLAYER_HIT)
-            sound.play(GameAssets.SOUND_PLAYER_HIT, {volume: 0.5})
     }
 }
